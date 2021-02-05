340c64e28082281a037b7834a05ebc4eb55064fe
ViCabral <vinicius.cabral@infojrunesp.com.br>
2021-02-05 10:58:13 -0300

    Array Multidimensional

---
+++ Introduction/src/Array1.java
@@ -0,0 +1,18 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+        int [] idades = {0, 1, 2, 5, 7};
+        int [] idadess = new int[]{1, 8, 9};
+        idade[0] = 5;
+        idade[1] = 10;
+
+        //System.out.println(idade[1]);
+
+        for (int i:
+             idades){
+            System.out.println(i);
+            
+        }
+
+    }
+}
